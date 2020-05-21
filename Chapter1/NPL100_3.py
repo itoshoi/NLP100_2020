393211b1ef3b03ddc6ed5070093be2cb1d84f5d8
itoshoi <uranun313@gmail.com>
2020-05-22 06:46:13 +0900

    Chapter1に問題文を追加

--- Chapter1/NPL100_3.py
+++ Chapter1/NPL100_3.py
@@ -1,3 +1,9 @@
+'''
+03. 円周率
+“Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.”
+という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+'''
+
 import re
 string = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 sp_string = re.findall('[^,. ]+', string)
--- Chapter1/NPL100_6.py
+++ Chapter1/NPL100_6.py
@@ -1,3 +1,10 @@
+'''
+06. 集合
+“paraparaparadise”と”paragraph”に含まれる文字bi-gramの集合を，
+それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．
+さらに，’se’というbi-gramがXおよびYに含まれるかどうかを調べよ
+'''
+
 import NPL100_5
 
 test_str1 = "paraparaparadise"
